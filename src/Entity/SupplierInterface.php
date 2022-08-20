87295400a6baf839239100d97eb549709337afe0
Audrius Nen <audriusnen@gmail.com>
2022-08-20 18:02:18 +0300

    Create SupplierInterface.php

---
+++ src/Entity/SupplierInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+interface SupplierInterface extends ResourceInterface 
+{
+    public function getName(): string;
+
+    public function setName(string $name): void;
+
+    public function getEmail(): string;
+
+    public function setEmail(string $email): void;
+}
+
